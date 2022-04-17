1a148982305ea49f37ebefa43fa3255e78c490f5
Babojack <jey.afandiyev@gmail.com>
2022-04-17 19:43:00 +0200

    Starting project

---
+++ amazon/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <h1>Hello guys! lets build amazon store</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAF1M8_8sKQDrrOBZBTvsXm3kp8mcuc8fw",
+  authDomain: "app-26316.firebaseapp.com",
+  projectId: "app-26316",
+  storageBucket: "app-26316.appspot.com",
+  messagingSenderId: "394133443173",
+  appId: "1:394133443173:web:92305d118724de2ee62c8c"
+};
