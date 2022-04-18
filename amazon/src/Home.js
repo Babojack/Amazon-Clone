a55a5db7ee0a88108f479cd708efd1ef1d49359d
Babojack <jey.afandiyev@gmail.com>
2022-04-18 14:34:46 +0200

    Starting with the main-page

---
+++ amazon/src/Home.js
@@ -0,0 +1,32 @@
+import "./Home.css";
+import React from "react";
+import Product from './Product.js'
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://m.media-amazon.com/images/I/61ATFA5aSoL._SX3740_.jpg"
+          alt=""
+        />
+        <div className="div home_row">
+          <Product />
+          <Product />
+        </div>
+
+        <div className="div home_row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="div home_row">
+          {/* Product */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ amazon/src/Product.js
@@ -0,0 +1,23 @@
+import './Product.css';
+import React from 'react'
+
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="div product-info">
+        <p>The lean Startup</p>
+        <p className="product-prise">
+          <small>€</small>
+          <strong>19,99</strong>
+        </p>
+      </div>
+      <div className="product-rating">
+        <p></p>
+      </div>
+      <img src="https://m.media-amazon.com/images/I/71gGKDXdoPL._AC_SX679_.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Product
