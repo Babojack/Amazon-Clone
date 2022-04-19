b61731cd16915c83eb0a55a39fb19e53fb434ca4
Babojack <jey.afandiyev@gmail.com>
2022-04-19 14:00:53 +0200

    .

--- amazon/src/Checkout.js
+++ amazon/src/Checkout.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 import "./Checkout.css";
-
 
 function Checkout() {
   return (
-    <div className="Checkout">
-      
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          className="checkout_ad"
+          src="https://cdn.mos.cms.futurecdn.net/eNrbgY7SGEixCodPTGsnMJ-1024-80.jpg.webp"
+          alt=""
+        />
+        <div>
+          <h2 className="checkout_title">Your shopping Basket</h2>
+        </div>
+      </div>
+      <div className="checkout_right">
+        <h2>The subtotal will go here</h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
--- amazon/src/Header.js
+++ amazon/src/Header.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/">
       <img
         className="header_logo"
         src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt=""
       />
+      </Link>
 
       <div className="header_search">
         <input className="header_searchInput" type="text" />
