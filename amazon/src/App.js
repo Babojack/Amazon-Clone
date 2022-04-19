f29cdc54ded401153d17903cf1ac24d436e519ef
Babojack <jey.afandiyev@gmail.com>
2022-04-19 12:59:35 +0200

    done

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
-import Header from './Header';
-import Home from './Home';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="App">
-      <Header />
-      <Home />
-      {/* Home */}
-      {/* Home */}
-      {/* Footer */}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+        <Route path="/checkout">
+            <Header />
+            <Checkout />
+
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+            </Switch>
+      </div>
+    </Router>
   );
 }
 
